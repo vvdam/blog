3301ed72b3e6d8ebbb3260d28568ec578629e172
vvdam <maaalouladam@gmail.com>
2023-11-15 20:13:49 +0100

    d

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function home()
+    {
+
+        return $this->render('login.html.twig');
+    }
+}
